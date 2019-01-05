12e847d37e2c003ac759b7a3220bcd9efebf219c
Mohammad Qabbani <qabbani454@gmail.com>
2019-01-05 23:23:55 +0200

    Upload Project

---
+++ services-index.php
@@ -0,0 +1,115 @@
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link href="css/services-index.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class=" col-lg-4 col-md-3 col-sm-6">
+                    <div class="services-box">
+                        <div class="icon-box"><img src="images/servicesImage/school-knowledge-concept.jpg" alt="img" width="50px;"height="50px;"></div>
+                        <div class="text-box">
+                            <h6><a href="">Knowledge & Consultation</a></h6>
+                            <p style="font-size: 12px; text-align: left; text-align: justify;">We start each project with an exploration where we meet with you to assess your needs.
+                                We conduct thorough investigation to understand your current problem and future vision. 
+                                We perform analysis of your target audience, collect information, 
+                                and study the challenge to provide strategic advice focused on using technology 
+                                to target your goals.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class=" col-lg-4 col-md-3 col-sm-6">
+                    <div class="services-box">
+                        <div class="icon-box"><img src="images/servicesImage/ux.png" alt="img" width="50px;"height="50px;"></div>
+                        <div class="text-box">
+                            <h6><a href="">Design & User Experience</a></h6>
+                            <p style="font-size: 12px; text-align: left; text-align: justify;">
+                                Providing your customers and clients with instant information about your company's 
+                                products and services is what separates your company from your competition.
+                                Upskills Group is dedicated to empowering your business to reach 
+                                the widest possible audience through the development of portals & websites
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class=" col-lg-4 col-md-3 col-sm-6">
+                    <div class="services-box">
+                        <div class="icon-box"><img src="images/servicesImage/software-development.png" alt="img" width="50px;"height="50px;"></div>
+                        <div class="text-box">
+                            <h6><a href="">Technology & Development</a></h6>
+                            <p style="font-size: 12px; text-align: left;text-align: justify;">
+                                Upskills has vast experience in developing multilingual websites and continuously 
+                                enhances the user experience of multilingual websites. We develop websites to be 
+                                fully accessible in many languages without compromising usability features, quality 
+                                and standards. We make it easy for you to translate and maintain</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-3 col-sm-6">
+                    <div class="services-box">
+                        <div class="icon-box"><img src="images/servicesImage/icon_technical.jpg" alt="img" width="50px;"height="50px;"></div>
+                        <div class="text-box">
+                            <h6><a href="">Support & Improvment</a></h6>
+                            <p style="font-size: 12px;text-align:left;text-align: justify;">
+                                Fulfilling and maximizing your website performance goals
+                                Our website support and improvement team monitors configure and analyze specific 
+                                performance scenarios for your site. We apply the necessary optimization to the 
+                                system to ensure best performance results and reduce the time your website takes 
+                                to respond to visitors requests.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-3 col-sm-6">
+                    <div class="services-box">
+                        <div class="icon-box"><img src="images/servicesImage/map.jpeg" alt="img" width="50px;"height="50px;"></div>
+                        <div class="text-box">
+                            <h6><a href="#">Mobail App</a></h6>
+                            <p style="font-size: 12px;text-align: left;text-align: justify;">
+                                Upskills Group provides mobile application development on all leading mobile platforms 
+                                including Android, iPhone, and Windows Mobile.
+                                Our experience in mobile development enabled us to offer various services to our 
+                                customers including proof of concept, feasibility studies, requirements analysis, 
+                                review of code and design.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-3 col-sm-6">
+                    <div class="services-box">
+                        <div class="icon-box"><img src="images/servicesImage/e-co.png" alt="img" width="50px;"height="50px;"></div>
+                        <div class="text-box">
+                            <h6><a href="#">E-Commerce Website</a></h6>
+                            <p style="font-size: 12px;text-align: left;text-align: justify;">
+                                Reach a wider audience with seamless translation capabilities
+                                Grow your online business and sales
+                                Go E-commerce with Upskills! We leverage many of the best modules to provide you with a 
+                                usable e-commerce site that is easily integrated with various solutions. You can now 
+                                manage your online products and informational website all in one place.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-3 col-sm-6">
+                    <div class="services-box">
+                        <div class="icon-box"><img src="images/servicesImage/seo-logo-.png" alt="img" width="50px;"height="50px;"></div>
+                        <div class="text-box">
+                            <h6><a href="#">SEO</a></h6>
+                            <p style="font-size: 12px;text-align: left;text-align: justify;">
+                                Optimizing your website to rank better by search engines and increasing visibility
+                                It’s not a fault of customers not to know that even good looking and user-friendly 
+                                websites don’t guarantee them a high traffic, but it’s a fault of web agencies not 
+                                to warn about it. We follow our full onsite search engine optimization checklist, 
+                                with several steps to be taken to ensure that the website is index-able and easily 
+                                reachable by search engines in order to improve the search ranking.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
